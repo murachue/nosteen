361b57f007010d9d814e606238a38a2af46cdd89
Murachue <392264+murachue@users.noreply.github.com>
2023-06-19 23:24:21 +0900

    move

--- src/routes/preferences.tsx
+++ src/routes/preferences.tsx
@@ -7,7 +7,7 @@ import invariant from "tiny-invariant";
 import TextInput from "../components/textinput";
 import { NostrWorker, useNostrWorker } from "../nostrworker";
 import state from "../state";
-import { contactsjsoncontent, expectn, metadatajsoncontent, rescue, sha256str, shortstyle } from "../util";
+import { broadcast, contactsjsoncontent, expectn, metadatajsoncontent, rescue, sha256str, shortstyle, signevent } from "../util";
 import { MuxPub } from "../pool";
 
 const MultiInput: FC<Omit<Parameters<typeof TextInput>[0], "value" | "onChange"> & {
@@ -72,57 +72,6 @@ function rot<T>(value: T, list: T[]) {
     const i = list.indexOf(value);
     return list[0 <= i && i < list.length - 1 ? i + 1 : 0];
 }
-
-type RelayPosts = {
-    event: Event;
-    postAt: number;
-    postByRelay: Map<string, null | { relay: string; recvAt: number; ok: boolean; reason: string; }>;
-    pub: MuxPub;
-};
-const broadcast = (noswk: NostrWorker, event: Event, onRealize: (repo: RelayPosts) => void, relays?: string[]): RelayPosts => {
-    const postAt = Date.now();
-    const post = noswk.postEvent(event, relays);
-
-    const repo: RelayPosts = {
-        event,
-        postAt,
-        postByRelay: new Map(post.relays.map(r => [utils.normalizeURL(r.relay.url), null])),
-        pub: post.pub,
-    };
-    post.pub.on("ok", recv => {
-        const recvAt = Date.now();
-        for (const r of recv) {
-            repo.postByRelay.set(utils.normalizeURL(r.relay.url), { relay: r.relay.url, recvAt, ok: true, reason: r.reason });
-        }
-        onRealize(repo);
-    });
-    post.pub.on("failed", recv => {
-        const recvAt = Date.now();
-        repo.postByRelay.set(utils.normalizeURL(recv.relay), { relay: recv.relay, recvAt, ok: false, reason: String(recv.reason) });
-        onRealize(repo);
-    });
-    // TODO: timeout? pub.on("forget", () => { });
-
-    // repo.pub.forget() is callers responsibility.
-    return repo;
-};
-const signevent = async (account: null | { pubkey: string; } | { privkey: string; }, tev: EventTemplate) => {
-    if (account && "privkey" in account) {
-        return finishEvent(tev, account.privkey);
-    } else if (window.nostr?.signEvent) {
-        const sev = await window.nostr.signEvent(tev);
-        if (sev.pubkey !== account?.pubkey) {
-            throw new Error(`NIP-07 set unexpected pubkey: pk=${sev.pubkey}, expected=${account?.pubkey}`);
-        }
-        return sev;
-    } else {
-        throw new Error("could not sign: no private key nor NIP-07 signEvent");
-    }
-};
-const emitevent = async (noswk: NostrWorker, account: null | { pubkey: string; } | { privkey: string; }, tev: EventTemplate, onRealize: (repo: ReturnType<typeof broadcast>) => void) => {
-    const event = await signevent(account, tev);
-    return broadcast(noswk, event, onRealize);
-};
 
 function useEventSnapshot<T>(subscribe: (onStoreChange: () => void) => () => void, getSnapshot: () => T): T {
     const [value, setValue] = useState(() => getSnapshot());
--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,10 @@
 import { sha256 } from "@noble/hashes/sha256";
 import { bytesToHex } from "@noble/hashes/utils";
-import { Event, nip19 } from "nostr-tools";
+import { Event, EventTemplate, finishEvent, nip19, utils } from "nostr-tools";
 import { CSSProperties } from "react";
 import { ContactsContent, DeletableEvent, MetadataContent, Post } from "./types";
+import { MuxPub } from "./pool";
+import { NostrWorker } from "./nostrworker";
 
 export const bsearchi = <T>(arr: T[], comp: (x: T) => boolean): number => {
     let left = 0;
@@ -128,6 +130,60 @@ export const contactsjsoncontent = (ev: DeletableEvent): ContactsContent | null 
         return json as ContactsContent;
     }
     return null;
+};
+
+export type RelayPosts = {
+    event: Event;
+    postAt: number;
+    postByRelay: Map<string, null | { relay: string; recvAt: number; ok: boolean; reason: string; }>;
+    pub: MuxPub;
+};
+
+export const broadcast = (noswk: NostrWorker, event: Event, onRealize: (repo: RelayPosts) => void, relays?: string[]): RelayPosts => {
+    const postAt = Date.now();
+    const post = noswk.postEvent(event, relays);
+
+    const repo: RelayPosts = {
+        event,
+        postAt,
+        postByRelay: new Map(post.relays.map(r => [utils.normalizeURL(r.relay.url), null])),
+        pub: post.pub,
+    };
+    post.pub.on("ok", recv => {
+        const recvAt = Date.now();
+        for (const r of recv) {
+            repo.postByRelay.set(utils.normalizeURL(r.relay.url), { relay: r.relay.url, recvAt, ok: true, reason: r.reason });
+        }
+        onRealize(repo);
+    });
+    post.pub.on("failed", recv => {
+        const recvAt = Date.now();
+        repo.postByRelay.set(utils.normalizeURL(recv.relay), { relay: recv.relay, recvAt, ok: false, reason: String(recv.reason) });
+        onRealize(repo);
+    });
+    // TODO: timeout? pub.on("forget", () => { });
+
+    // repo.pub.forget() is callers responsibility.
+    return repo;
+};
+
+export const signevent = async (account: null | { pubkey: string; } | { privkey: string; }, tev: EventTemplate) => {
+    if (account && "privkey" in account) {
+        return finishEvent(tev, account.privkey);
+    } else if (window.nostr?.signEvent) {
+        const sev = await window.nostr.signEvent(tev);
+        if (account?.pubkey && sev.pubkey !== account.pubkey) {
+            throw new Error(`NIP-07 set unexpected pubkey: pk=${sev.pubkey}, expected=${account.pubkey}`);
+        }
+        return sev;
+    } else {
+        throw new Error("could not sign: no private key nor NIP-07 signEvent");
+    }
+};
+
+export const emitevent = async (noswk: NostrWorker, account: null | { pubkey: string; } | { privkey: string; }, tev: EventTemplate, onRealize: (repo: ReturnType<typeof broadcast>) => void, relays?: string[]) => {
+    const event = await signevent(account, tev);
+    return broadcast(noswk, event, onRealize, relays);
 };
 
 export const NeverMatch = /(?!)/;
